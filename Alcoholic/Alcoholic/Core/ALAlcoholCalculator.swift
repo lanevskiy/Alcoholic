e766594c7f7cf44e3055bf6a424e17cc352605b6
Admin <admin@Admins-Mac.local>
2017-03-21 01:19:17 +0300

    model added

--- Alcoholic/Alcoholic/Core/ALAlcoholCalculator.swift
+++ Alcoholic/Alcoholic/Core/ALAlcoholCalculator.swift
@@ -13,3 +13,26 @@ enum IntoxicationStage : Int {
     case middle = 1
     case hard = 2
 }
+
+enum AlcoholType {
+    enum SpiritType : String {
+        case vodka, brandy, rum, tequila, whiskey, gin
+    }
+    
+    enum WineType : String {
+        case tableWine, fortifiedWine, fruitWine, sangria, sparklingWine
+    }
+    
+    enum BeerType : String {
+        case ale, fruitBeer, lager, sahti, wheatBeer
+    }
+}
+
+struct Alcohol {
+    let spirit  : (alcohol: AlcoholType.SpiritType, bottles: Int)?
+    let beer    : (alcohol: AlcoholType.BeerType,   bottles: Int)?
+    let wine    : (alcohol: AlcoholType.WineType,   bottles: Int)?
+}
+
+
+
